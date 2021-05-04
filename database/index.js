cb346582bcc5afdd548cfc9ab15eb7e5c1c84696
Kimberly Kung <kimberly.kung@gmail.com>
2021-05-03 20:07:55 -0700

    Updated to start to seed my DB.

--- database/index.js
+++ database/index.js
@@ -1,140 +1,39 @@
-const seedDB = require('./seeder.js');
+const {seed} = require('./seeder.js');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/overview', {useNewUrlParser: true, useUnifiedTopology: true});
-module.exports.db = mongoose.connection;
+mongoose.connect('mongodb://localhost/overview', { useNewUrlParser: true, useUnifiedTopology: true });
+db = mongoose.connection;
 
-const testConnection = () => {
-  module.exports.db.on('error', () => {
+const testConnection = async () => {
+  db.on('error', () => {
     console.log('ERROR! DB no Connected...')
   });
-  module.exports.db.once('open', () => {
+  db.once('open', () => {
     console.log('DB Connected!!')
   });
 
   // should probs run a test to see if DB has been seeded
   //if not, seed db called seedDB
+  await Overview.find().exec()
+    .then(result => {
+      if (result.length === 0) {
+        console.log('overview empty');
+        seed();
+      }
+    })
 }
+
+// -----MODELS-----
+const Location = mongoose.model('Location');
+const Owner = mongoose.model('Owner');
+const Pricing = mongoose.model('Pricing');
+const Details = mongoose.model('Details');
+const Lodging = mongoose.model('Lodging');
+const Essentials = mongoose.model('Essentials');
+const Amenities = mongoose.model('Amenities');
+const Activities = mongoose.model('Activities');
+const Terrain = mongoose.model('Terrain');
+const Overview = mongoose.model('Overview');
+
 testConnection();
 
-// ----- SCHEMAS -----
-const locationSchema = new  mongoose.Schema({
-  name: String,
-  address: String
-});
-
-const ownerSchema = new mongoose.Schema({
-  name: String,
-  imageUrl: String
-});
-
-const pricingSchema = new mongoose.Schema({
-  averagePricePerNight: Number,
-  cleaningFee: Number,
-  monthsOutForBooking: Number,
-  weeknightDiscount: Number,
-  instantBook: Boolean
-});
-
-const detailsSchema = new mongoose.Schema({
-  checkInTime: String,
-  checkOutTime: String,
-  cancellationPolicy: String,
-  onArrival: String,
-  responseTime: String,
-  responseRate: String
-});
-
-const lodgingSchema = new mongoose.Schema({
-  type: String,
-  numberOfSites: Number,
-  maxGuestsPerSite: Number,
-  ADAaccess: Boolean,
-  parking: Boolean
-});
-
-const essentialsSchema = new mongoose.Schema({
-  campfires: Boolean,
-  toilet: Boolean,
-  pets: Boolean
-});
-
-const amenitiesSchema = new mongoose.Schema({
-  potableWater: {
-    available: Boolean,
-    options: String,
-    description: String
-  },
-  kitchen: {
-    available: Boolean,
-    options: String,
-    description: String
-  },
-  shower: {
-    available: Boolean,
-    options: String,
-    description: String
-  },
-  picnicTable: {
-    available: Boolean,
-    description: String
-  },
-  wifi: {
-    available: Boolean,
-    description: String
-  },
-  bins: {
-    available: Boolean,
-    options: String,
-    description: String
-  }
-});
-
-const activitiesSchema = new mongoose.Schema({
-  biking: Boolean,
-  fishing: Boolean,
-  hiking: Boolean,
-  horsebackRiding: Boolean,
-  boating: Boolean,
-  offRoading: Boolean,
-  whitewaterPaddling: Boolean,
-  climbing: Boolean,
-  snowsports: Boolean,
-  surfing: Boolean,
-  windsports: Boolean,
-  swimming: Boolean,
-  paddling: Boolean,
-  wildlifeWatching: Boolean,
-  surfing: Boolean
-});
-
-const terrainSchema = new mongoose.Schema({
-  lake: Boolean,
-  beach: Boolean,
-  forest: Boolean,
-  reviewStreamCreek: Boolean,
-  hotSpring: Boolean,
-  swimmingHole: Boolean,
-  desert: Boolean,
-  cave: Boolean,
-  waterfall: Boolean,
-  driveway: Boolean
-});
-
-const overviewSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  location: locationSchema,
-  owner: ownerSchema,
-  pricing: pricingSchema,
-  details: detailsSchema,
-  lodging: lodgingSchema,
-  essentials: essentialsSchema,
-  amenities: amenitiesSchema,
-  activities: activitiesSchema,
-  terrain: terrainSchema
-})
-
-module.exports = {
-
-}
--- database/seeder.js
+++ database/seeder.js
@@ -1,8 +1,184 @@
 const faker = require('faker');
-const { db } = require('./index.js');
+// const database = require('./index');
+// console.log(database)
+const mongoose = require('mongoose');
 
-module.exports.seed = async () => {
-  for (let i = 0; i < 100; i++) {
+mongoose.connect('mongodb://localhost/overview', { useNewUrlParser: true, useUnifiedTopology: true });
+db = mongoose.connection;
+try {
+  db.on('error', () => {
+    throw Error('DB Not Connected!')
+  });
+  db.once('open', () => {
+    console.log('DB Connected in seeder')
+  });
+} catch (error) {
+  console.log('ERROR: ', error);
+}
 
+// ----- SCHEMAS -----
+
+const locationSchema = new mongoose.Schema({
+  name: String,
+  address: String
+});
+
+const ownerSchema = new mongoose.Schema({
+  name: String,
+  imageUrl: String
+});
+
+const pricingSchema = new mongoose.Schema({
+  averagePricePerNight: Number,
+  cleaningFee: Number,
+  monthsOutForBooking: Number,
+  weeknightDiscount: Number,
+  instantBook: Boolean
+});
+
+const detailsSchema = new mongoose.Schema({
+  checkInTime: String,
+  checkOutTime: String,
+  cancellationPolicy: String,
+  onArrival: String,
+  responseTime: String,
+  responseRate: String
+});
+
+const lodgingSchema = new mongoose.Schema({
+  type: String,
+  numberOfSites: Number,
+  maxGuestsPerSite: Number,
+  ADAaccess: Boolean,
+  parking: Boolean
+});
+
+const essentialsSchema = new mongoose.Schema({
+  campfires: Boolean,
+  toilet: Boolean,
+  pets: Boolean
+});
+
+const amenitiesSchema = new mongoose.Schema({
+  potableWater: {
+    available: Boolean,
+    options: String,
+    description: String
+  },
+  kitchen: {
+    available: Boolean,
+    options: String,
+    description: String
+  },
+  shower: {
+    available: Boolean,
+    options: String,
+    description: String
+  },
+  picnicTable: {
+    available: Boolean,
+    description: String
+  },
+  wifi: {
+    available: Boolean,
+    description: String
+  },
+  bins: {
+    available: Boolean,
+    options: String,
+    description: String
   }
+});
+
+const activitiesSchema = new mongoose.Schema({
+  biking: Boolean,
+  fishing: Boolean,
+  hiking: Boolean,
+  horsebackRiding: Boolean,
+  boating: Boolean,
+  offRoading: Boolean,
+  whitewaterPaddling: Boolean,
+  climbing: Boolean,
+  snowsports: Boolean,
+  surfing: Boolean,
+  windsports: Boolean,
+  swimming: Boolean,
+  paddling: Boolean,
+  wildlifeWatching: Boolean,
+  surfing: Boolean
+});
+
+const terrainSchema = new mongoose.Schema({
+  lake: Boolean,
+  beach: Boolean,
+  forest: Boolean,
+  reviewStreamCreek: Boolean,
+  hotSpring: Boolean,
+  swimmingHole: Boolean,
+  desert: Boolean,
+  cave: Boolean,
+  waterfall: Boolean,
+  driveway: Boolean
+});
+
+const overviewSchema = new mongoose.Schema({
+  siteId: Number,
+  name: String,
+  description: String,
+  location: locationSchema,
+  owner: ownerSchema,
+  pricing: pricingSchema,
+  details: detailsSchema,
+  lodging: lodgingSchema,
+  essentials: essentialsSchema,
+  amenities: amenitiesSchema,
+  activities: activitiesSchema,
+  terrain: terrainSchema
+})
+
+// -----MODELS-----
+const Location = mongoose.model('Location', locationSchema);
+const Owner = mongoose.model('Owner', ownerSchema);
+const Pricing = mongoose.model('Pricing', pricingSchema);
+const Details = mongoose.model('Details', detailsSchema);
+const Lodging = mongoose.model('Lodging', lodgingSchema);
+const Essentials = mongoose.model('Essentials', essentialsSchema);
+const Amenities = mongoose.model('Amenities', amenitiesSchema);
+const Activities = mongoose.model('Activities', activitiesSchema);
+const Terrain = mongoose.model('Terrain', terrainSchema);
+const Overview = mongoose.model('Overview', overviewSchema);
+
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+module.exports.seed = async () => {
+  await db.dropCollection('owners')
+    .then(result => {
+      console.log('owners dropped ', result);
+    })
+    .catch(error => {
+      console.log('error: ', error)
+    })
+
+  for (let i = 0; i < 5; i++) {
+    //---OWNER---
+    const imageUrl = faker.image.avatar();
+    console.log('owner pic: ', imageUrl);
+    const name = faker.name.firstName() + ' ' + alphabet[Math.floor(Math.random() * alphabet.length)] + '.';
+    console.log('owner name: ', name);
+    const newOwner = {
+      name: name,
+      imageUrl: imageUrl
+    }
+    await Owner.create(newOwner)
+      .then(result => {
+        console.log('Owner Saved: ', result);
+      })
+      .catch(err => {
+        console.log('error creating doc: ', err)
+      })
+  }
+  // const address = faker.address.streetAddress(true);
+  // console.log('Address: ', address);
+
+
+
 };
