ed3c190b5765ecb779a37a9d69b9c2e0c96098c9
Kimberly Kung <kimberly.kung@gmail.com>
2021-05-10 20:23:28 -0700

    WROTE FIRST TEST AND IT PASSED!!!! and i got jest to work.

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  verbose: true,
-  testEnvironment: 'node'
-};
-
-module.exports = config;
-
-
--- testing/api.spec.js
+++ testing/api.spec.js
@@ -1,16 +1,28 @@
 const database = require('../database/index.js');
+const mongoose = require('mongoose');
 
-test('overview returns object', () => {
-  expect(database.generalLookup(0)).toBe(expect.objectContaining({
-    name: expect.any(String),
-    location: expect.objectContaining({
+beforeAll(async () => {
+  await mongoose.connect('mongodb://127.0.0.1/overview', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
+});
+
+afterAll(async done => {
+  await mongoose.connection.close();
+  done();
+})
+
+test('overview returns object',async () => {
+  expect(await database.generalLookup(0)).toStrictEqual(
+    expect.objectContaining({
       name: expect.any(String),
-      address: expect.any(String),
-      numberOfSites: expect.any(Number)
-    }),
-    owner: expect.objectContaining({
-      name: expect.any(String),
-      imageUrl: expect.any(String)
+      location: expect.objectContaining({
+        name: expect.any(String),
+        address: expect.any(String),
+        numberOfSites: expect.any(Number)
+      }),
+      owner: expect.objectContaining({
+        name: expect.any(String),
+        imageUrl: expect.any(String)
+      })
     })
-  }));
+  );
 })
