f85dd5fd9cea066f03ef1dadf69d3348579b38cb
kimberly kung <kimberly.kung@gmail.com>
2021-05-25 19:02:13 -0700

    Added error handling on header component

--- client/Components/header.jsx
+++ client/Components/header.jsx
@@ -23,7 +23,17 @@ class Header extends React.Component {
       )
     } else {
       console.log('Not connected to reviews service');
-      return;
+      return (
+        <div className='header'>
+          <div className='header-buttons'>
+            <div className='upload'>
+              <a className='button upload-photo'><span className='icon fa fa-camera'></span> Upload</a>
+            </div>
+            <a className='button save-listing'>Save to list<i className="fas fa-chevron-down chevron"></i></a>
+            <a className='button share'><span className='icon hc-awesome-share-apple'/></a>
+          </div>
+        </div>
+      )
     }
   }
 }
