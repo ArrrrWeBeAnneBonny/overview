05c6806d597df3d396bdad23a169bad7c3756385
kimberly <kimberly.kung@gmail.com>
2021-06-03 14:42:06 -0700

    Moved sticky header content to center

--- client/Components/stickyheader.jsx
+++ client/Components/stickyheader.jsx
@@ -31,29 +31,31 @@ class StickyHeader extends React.Component {
   render() {
     // console.log(this.props)
     return (
-      <div id='sticky-overview' className='sticky-header sticky--show main'>
-        <div className='two-thirds'>
-          <h2 className='name'>{this.props.siteName}{this.props.location.verified && <span className='verified-site fa fa-check' />}</h2>
+      <div className='sticky-header sticky--show'>
+        <div className='main'>
+          <div className='two-thirds'>
+            <h2 className='name'>{this.props.siteName}{this.props.location.verified && <span className='verified-site fa fa-check' />}</h2>
 
-          <div className='sticky-header-button'>
-            {// checking error in accessing review service
-              !this.props.header.errorOccured
-                ? this.showRecommendation()
-                : <div className='empty' />
-            }
+            <div className='sticky-header-button'>
+              {// checking error in accessing review service
+                !this.props.header.errorOccured
+                  ? this.showRecommendation()
+                  : <div className='empty' />
+              }
 
-            <div className='sticky-header-buttons'>
-              <div className='upload'>
-                <a className='button upload-photo' onClick={this.openModal}><span className='icon fa fa-camera'></span> Upload</a>
+              <div className='sticky-header-buttons'>
+                <div className='upload'>
+                  <a className='button upload-photo' onClick={this.openModal}><span className='icon fa fa-camera'></span> Upload</a>
+                </div>
+                <a className='button save-listing' onClick={this.openModal}>Save to list<i className="fas fa-chevron-down chevron"></i></a>
+                <a className='button share' onClick={this.openModal}><span className='icon hc-awesome-share-apple' /></a>
               </div>
-              <a className='button save-listing' onClick={this.openModal}>Save to list<i className="fas fa-chevron-down chevron"></i></a>
-              <a className='button share' onClick={this.openModal}><span className='icon hc-awesome-share-apple' /></a>
             </div>
           </div>
-        </div>
-        <div className='two-thirds'>
-          <div className='sticky-header-nav-bar'>
+          <div className='two-thirds'>
+            <div className='sticky-header-nav-bar'>
 
+            </div>
           </div>
         </div>
       </div>
