72680d60c8eefea5a351608957085506e6734d9e
kimberly <kimberly.kung@gmail.com>
2021-06-11 08:18:56 -0700

    small changes to config and url

--- server/config.js
+++ server/config.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   production: {
     overview: 'http://ec2-35-163-3-32.us-west-2.compute.amazonaws.com/',
-    reviews: 'http://localhost:3001/',
+    reviews: 'http://ec2-54-193-152-3.us-west-1.compute.amazonaws.com/',
     booking: 'http://localhost:3002/',
     photogallery: 'http://localhost:3004/'
   },
